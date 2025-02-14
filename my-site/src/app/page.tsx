4e134f3917daff43f963634122624e39fec458d2
Cyrus Wickersham <cyruswickersham@Cyruss-MacBook-Air.local>
2025-02-14 09:33:21 -0500

    removed some boilerplate, added a couple more apps

--- my-site/src/app/page.tsx
+++ my-site/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li>Cyrus&apos; portfolio :-D</li>
+          <ul>Cyrus&apos; portfolio :-D</ul>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -17,6 +17,33 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Weather app 
+          </a>
+
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Book Tracker 
+          </a>
+
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Data Pipeline Project
+          </a>
+
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mental Health Project
           </a>
         </div>
       </main>
