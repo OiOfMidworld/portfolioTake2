fc33c30e1083550d6e1942a3c3cd3a2759127d2a
OiOfMidworld <65954177+OiOfMidworld@users.noreply.github.com>
2025-11-22 12:28:27 -0500

    Update links and labels in page.tsx

--- my-site/src/app/page.tsx
+++ my-site/src/app/page.tsx
@@ -20,29 +20,29 @@ export default function Home() {
 
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href=""
+            href="https://github.com/OiOfMidworld/ooi_fair_pipeline_project"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Book Tracker 
+             OOI FAIR Pipeline Project Repo 
           </a>
 
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href=""
+            href="https://github.com/OiOfMidworld"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Data Pipeline Project
+            Github
           </a>
 
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href=""
+            href="https://linkedin.com/in/cyruswickersham"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Mental Health Project
+            Linkedin
           </a>
         </div>
       </main>
