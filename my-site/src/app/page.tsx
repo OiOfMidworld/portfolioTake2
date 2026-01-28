3e4ad7d273db566016e395415c5158474f7c83c0
OiOfMidworld <65954177+OiOfMidworld@users.noreply.github.com>
2026-01-28 14:28:32 -0500

    Add link to DJ FITZ website in page.tsx

--- my-site/src/app/page.tsx
+++ my-site/src/app/page.tsx
@@ -17,7 +17,14 @@ export default function Home() {
           >
             Weather app 
           </a>
-
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://djfitz.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            DJ FITZ 
+          </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://github.com/OiOfMidworld/ooi_fair_pipeline_project"
